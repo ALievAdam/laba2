47c86a7f89da1cf83850cb0262dae52c88e852be
ALievAdam <90550031+ALievAdam@users.noreply.github.com>
2021-11-20 19:18:31 +0500

    изменил программу

--- лаба2/лаба2/лаба2.cpp
+++ лаба2/лаба2/лаба2.cpp
@@ -1,85 +1,163 @@
 ﻿#include <iostream>
+#include<math.h>
+#include <string>
 #include <conio.h>
+
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "Russian");
-    int a;
-    cout << "Введите число: ";
-    cin >> a;
-    cout << "Ваше число: " << " ";
+int main()
+{
+	setlocale(LC_ALL, "rus");
 
-    if (a < 0) {
-        cout << "Отрицательное число";
-    }
-    switch (a / 1000)
-    {
-    case 1: cout << "одна тысяча "; break;
-    case 2: cout << "две тысячи "; break;
-    case 3: cout << "три тысячи "; break;
-    case 4: cout << "четыре тысячи "; break;
-    case 5: cout << "пять тысяч "; break;
-    case 6: cout << "шесть тысяч "; break;
-    case 7: cout << "семь тысяч "; break;
-    case 8: cout << "восемь тысяч "; break;
-    case 9: cout << "девять тысяч "; break;
-    }
+	int i;
+	string a, b, c, s;
+	a = "рубль";
+	b = "рубля";
+	c = "рублей";
+	s = ' ';
 
-    switch ((a % 1000) / 100)
-    {
-    case 1: cout << "сто "; break;
-    case 2: cout << "двести "; break;
-    case 3: cout << "триста "; break;
-    case 4: cout << "четыреста "; break;
-    case 5: cout << "пятьсот "; break;
-    case 6: cout << "шестьсот "; break;
-    case 7: cout << "семьсот "; break;
-    case 8: cout << "восемьсот "; break;
-    case 9: cout << "девятьсот "; break;
-    }
+	cout << "Введите число от 1 до 9999\n\n";
+	cin >> i;
 
-    switch ((a % 100) / 10)
-    {
-    case 2: cout << "двадцать "; break;
-    case 3: cout << "тридцать "; break;
-    case 4: cout << "сорок "; break;
-    case 5: cout << "пятьдесят "; break;
-    case 6: cout << "шестьдесят "; break;
-    case 7: cout << "семьдесят "; break;
-    case 8: cout << "восемьдесят "; break;
-    case 9: cout << "девяносто "; break;
-    }
+	while (i < 1 || i>9999)
 
-    if (a % 100 >= 10 & a % 100 < 20) {
-        switch (a % 100) {
-        case 10: cout << "десять рублей"; break;
-        case 11: cout << "одинадцать рублей"; break;
-        case 12: cout << "двенадцать рублей"; break;
-        case 13: cout << "тринадцать рублей"; break;
-        case 14: cout << "четырнадцать рублей"; break;
-        case 15: cout << "пятнадцать рублей"; break;
-        case 16: cout << "шестнадцать рублей"; break;
-        case 17: cout << "семнадцать рублей"; break;
-        case 18: cout << "восемнадцать рублей"; break;
-        case 19: cout << "девятнадцать рублей"; break;
-        }
-    }
-    else {
-        switch (a % 10)
-        {
-        case 0: cout << " рублей\n"; break;
-        case 1: cout << "один рубль\n"; break;
-        case 2: cout << "два рубля\n"; break;
-        case 3: cout << "три рубля\n"; break;
-        case 4: cout << "четыре рубля"; break;
-        case 5: cout << "пять рублей"; break;
-        case 6: cout << "шесть рублей"; break;
-        case 7: cout << "семь рублей"; break;
-        case 8: cout << "восемь рублей"; break;
-        case 9: cout << "девять рублей"; break;
-        }
-    }
-    _getch();
+	{
+		cout << "Введите число или введенное вами число не принадлежит (0; 9999)." << endl;
+		break;
+	}
 
-    return 0;
+	while (i > 0 && i < 10000)
+	{
+		if (i % 1000 % 100 % 10 < 10) // Подсчёт единиц
+		{
+			switch (i % 1000 % 100 % 10)
+			{
+			case 0: s = " " + c;
+				break;
+			case 1: s = "один " + a;
+				break;
+			case 2: s = "два " + b;
+				break;
+			case 3: s = "три " + b;
+				break;
+			case 4: s = "четыре " + b;
+				break;
+			case 5: s = "пять " + c;
+				break;
+			case 6: s = "шесть " + c;
+				break;
+			case 7: s = "семь " + c;
+				break;
+			case 8: s = "восемь " + c;
+				break;
+			case 9: s = "девять " + c;
+				break;
+			}
+		}
+		if (9 < i % 1000 % 100 < 20) // Подсчёт от 10 до 20
+		{
+			switch (i % 1000 % 100)
+			{
+			case 10: s = "десять " + c;
+				break;
+			case 11: s = "одиннадцать " + c;
+				break;
+			case 12: s = "двенадцать " + c;
+				break;
+			case 13: s = "тринадцать " + c;
+				break;
+			case 14: s = "четырнадцать  " + c;
+				break;
+			case 15: s = "пятнадцать  " + c;
+				break;
+			case 16: s = "шестнадцать " + c;
+				break;
+			case 17: s = "семнадцать " + c;
+				break;
+			case 18: s = "восемнадцать " + c;
+				break;
+			case 19: s = "девятнадцать " + c;
+				break;
+			}
+		}
+
+		if (10 < i % 1000 % 100 < 100) // Подсчёт десятков
+		{
+
+			switch (((i % 1000) % 100) / 10)
+			{
+			case 2: s = "двадцать " + s;
+				break;
+			case 3: s = "тридцать " + s;
+				break;
+			case 4: s = "сорок  " + s;
+				break;
+			case 5: s = "пятьдесят  " + s;
+				break;
+			case 6: s = "шестьдесят " + s;
+				break;
+			case 7: s = "семьдесят " + s;
+				break;
+			case 8: s = "восемьдесят " + s;
+				break;
+			case 9: s = "девяносто " + s;
+				break;
+			}
+		}
+
+		if (99 < i % 1000 < 1000) // Подсчёт сотен
+		{
+
+			switch ((i % 1000) / 100)
+			{
+			case 1: s = "сто " + s;
+				break;
+			case 2: s = "двести " + s;
+				break;
+			case 3: s = "триста " + s;
+				break;
+			case 4: s = "четыреста  " + s;
+				break;
+			case 5: s = "пятьсот  " + s;
+				break;
+			case 6: s = "шестьсот " + s;
+				break;
+			case 7: s = "семьсот " + s;
+				break;
+			case 8: s = "восемьсот " + s;
+				break;
+			case 9: s = "девятьсот " + s;
+				break;
+			}
+		}
+
+		if (0 < i / 1000 < 10) // Подсчёт тысяч
+		{
+
+			switch (i / 1000)
+			{
+			case 1: s = "одна тысяча " + s;
+				break;
+			case 2: s = "две тысячи " + s;
+				break;
+			case 3: s = "три тысячи " + s;
+				break;
+			case 4: s = "четыре тысячи  " + s;
+				break;
+			case 5: s = "пять тысяч  " + s;
+				break;
+			case 6: s = "шесть тысяч " + s;
+				break;
+			case 7: s = "семь тысяч " + s;
+				break;
+			case 8: s = "восемь тысяч " + s;
+				break;
+			case 9: s = "девять тысяч " + s;
+				break;
+			}
+		}
+		cout << "\n" << s;
+		_getch();
+		return 0;
+	}
 }
